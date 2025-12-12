3e756cf42f9f1c8cc631a1b339fc77efad859d10
sabbumia <dwmaster22@gmail.com>
2025-12-12 20:41:07 +0600

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        const { pathname } = req.nextUrl;
+        
+        // Public routes that don't require authentication
+        const publicRoutes = ["/", "/signin", "/signup", "/how-it-works"];
+        
+        if (publicRoutes.includes(pathname)) {
+          return true;
+        }
+        
+        // All other routes require authentication
+        return !!token;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - api/auth (auth endpoints)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public files
+     */
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|public).*)",
+  ],
+};
